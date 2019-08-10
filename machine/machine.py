34e5fc4a2f4b46c1f432d3e2fa81ef04e301d9d0
juan <juanse962>
2019-08-10 14:48:45 -0500

    sin Ñ

--- machine/machine.py
+++ machine/machine.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import unicodedata
 import maquina32
  
 def solicitarMaquina():
@@ -27,7 +26,7 @@ def seleccionarOpcion():
     while not salir:
         print("\n Seleccione una opcion")
         print("1. Calcular numero mas grande de la maquina")
-        print("2. Calcular numero mas pequeño de la maquina")
+        print("2. Calcular numero mas pequenio de la maquina")
         print("3. Calcular epsilon de la maquina")
         print("4. Convertir base 10 a numero de maquina")
         print("5. Convertir numero de maquina a base 10")

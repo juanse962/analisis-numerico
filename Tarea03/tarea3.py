bc3f31414b991f4269e76b3c0a821d6f1fde3574
juanse962 <jgomez40@eafit.edu.co>
2019-09-04 01:37:24 -0500

    Tarea03

--- Tarea03/tarea3.py
+++ Tarea03/tarea3.py
@@ -1,10 +1,11 @@
 from sympy import *
 import math
+
 x = symbols('x')
 y = symbols('y')
 z = symbols('z')
 
-def biseccion_circunferencia(intervalo,niter,tolerancia,function_circulo,v,a):
+def biseccion_(intervalo,niter,tolerancia,function_circulo,v,a):
 
     xi = intervalo[0][0][0]
     yi = intervalo[0][0][1]
@@ -12,55 +13,50 @@ def biseccion_circunferencia(intervalo,niter,tolerancia,function_circulo,v,a):
     xf = intervalo[0][1][0]
     yf = intervalo[0][1][1]
     zf = intervalo[0][1][2]
-    alphai = (x)
-    alphaf = 1
-    niter = niter
 
-    function_circulo = function_circulo
+    fxi_circulo = function_circulo.subs(x, xi).subs(y, yi).subs(z, zi)
+    fxf_circulo = function_circulo.subs(x, xf).subs(y, yf).subs(z, zf)
 
-    fxi_circulo = function_circulo.subs(x, xi).subs(y,yi).subs(z,zi)
-    fxf_circulo = function_circulo.subs(x, xf).subs(y,yf).subs(z,zf)
+    if fxi_circulo == 0:return[xi,yi.zi]
+    elif fxf_circulo == 0:return [xf,yf,zf]
+    elif fxi_circulo * fxf_circulo < 0:
 
-    if(fxi_circulo == 0):
-        return[xi,yi.zi]
-    elif (fxf_circulo == 0):
-        return [xf,yf,zf]
-    elif (fxi_circulo * fxf_circulo < 0):
         xm = (xi+xf)/2
         ym = (yi+yf)/2
         zm = (zi+zf)/2
-
-        fxm_circulo = function_circulo.subs(x, xm).subs(y,ym).subs(z,zm)
+        fxm_circulo = function_circulo.subs(x, xm).subs(y, ym).subs(z, zm)
         count = 1
         error = float(tolerancia) + 1
-        while (error > tolerancia) and (count < niter) and (fxm_circulo != 0):
-            if(fxi_circulo * fxm_circulo < 0):
+
+        while error > tolerancia and count < niter and fxm_circulo != 0:
+            if fxi_circulo * fxm_circulo < 0:
+
                 xf = xm
                 yf = ym
                 zf = zm
                 rf = recta_r3([(xi, yi, zi)], [(xf,yf, zf)],a,v)
-                fxf_circulo = function_circulo.subs(x, rf[0]).subs(y,rf[1]).subs(z,rf[2])
+                fxf_circulo = function_circulo.subs(x, rf[0]).subs(y, rf[1]).subs(z, rf[2])
 
             else:
+
                 xi = xm
                 yi = ym
                 zi = zm
-                ri = recta_r3([(xf, yf, zf)], [(xi,yi, zi)],a,v)               
-                fxi_circulo = function_circulo.subs(x,ri[0]).subs(y,ri[1]).subs(z,ri[2])  
+                ri = recta_r3([(xf, yf, zf)], [(xi,yi,zi)],a,v)               
+                fxi_circulo = function_circulo.subs(x, ri[0]).subs(y, ri[1]).subs(z, ri[2])
+
             xaux = xm  
             xm = (xi+xf)/2
             ym = (yi+yf)/2
             zm = (zi+zf)/2
-            rm = recta_r3([(xm, ym, zm)], [(0,0, 0)],a,v)
+            rm = recta_r3([(xm, ym, zm)], [(xm,ym, zm)],a,v)
             fxm_circulo = function_circulo.subs(x, rm[0]).subs(y,rm[1]).subs(z,rm[2])
             error = abs(xm-xaux)
             count += 1 
-        if(fxm_circulo == 0):
-            return [xm,ym,zm]
-        elif(error < tolerancia):
-            return[xm,ym,zm]
-        else:
-            print("Fracaso el numero de iteraciones: ",niter)    
+
+        if fxm_circulo == 0:return [xm,ym,zm]
+        elif error < tolerancia:return[xm,ym,zm]
+        else:print("Fracaso el numero de iteraciones: ",niter)    
 
 def recta_r3(punto0,punto1,alpha,v):
     
@@ -109,8 +105,9 @@ def raices_circunferencia(p0,p1,delta,niter):
     print("-----Raices en r3-----\n")
     function_esfera = x**2+y**2+z**2 - 4
     intervalos = busqueda_esfera(p0, p1, delta, niter, function_esfera)
+
     for i in range(len(intervalos[0])):
-        raiz = biseccion_circunferencia([intervalos[0][i]],niter,0.00048,function_esfera,intervalos[1],intervalos[2])
-        print ("Para el intervalo: {0} tiene una raiz en: {1}".format(intervalos[0][i],raiz))
+        raiz = biseccion_([intervalos[0][i]],niter,0.00048,function_esfera,intervalos[1],intervalos[2])
+        print ("Para el intervalo: {0} tiene una raiz en: {1}\n".format(intervalos[0][i],raiz))
 
 raices_circunferencia([-2.08,-0.19,0],[1.25,-0.59,1.45],0.2,100)
